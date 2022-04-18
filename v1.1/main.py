75b5e41c31d90c7d0330004073b0a6e333315abf
Knd <delend89@gmail.com>
2022-04-18 12:04:01 -0300

    sinapse

---
+++ v1.1/main.py
@@ -0,0 +1,19 @@
+from funcoes import *
+
+#se é dentro do estado de SP (1 ou 0)
+estado = int(input("Estado: "))
+
+#se é contribuinte ou não (1 ou 0)
+contribuinte = int(input("Contribuinte: "))
+origem = 1
+
+if contribuinte == 1:
+    # origem produto (1 ou 6)
+    origem = int(input("Origem: "))
+    f()
+#produto com ou sem IPI (1 ou 0)
+ipi = int(input("IPI: "))
+
+
+
+
