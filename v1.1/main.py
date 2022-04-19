f8a084a615753a5c3f9d3d5e158d20192a0727c2
Knd <delend89@gmail.com>
2022-04-19 14:18:56 -0300

    sinapse

--- v1.1/main.py
+++ v1.1/main.py
@@ -1,39 +1,48 @@
 from funcoes import *
 
+print("Calculadora TES - ver 1.1")
+print("Digite 1 para respoder \"sim\"")
+print("Digite 0 para respoder \"nao\"")
+print("------------------------------")
+
 #se é dentro do estado de SP (1 ou 0)
-estado = int(input("É de SP?: "))
+estado = int(input("Cliente é de SP?: "))
 
 #é pessoa fisica? (1 ou 0)
-cpf = int(input("tem CPF: "))
+cpf = int(input("É pessoa fisica?: "))
 
 contribuinte = 0
 
 if cpf == 0:
     # se é contribuinte ou não (1 ou 0)
-    contribuinte = int(input("Contribuinte: "))
+    contribuinte = int(input("É contribuinte?: "))
 
 
 origem = 1
 fs = 0
 if contribuinte == 1:
     # origem produto (1 ou 6)
-    origem = int(input("Origem produto: "))
+    origem = int(input("Digite a origem do produto (1/6): "))
 
     #fapesp ou ffm (0,1,2) (nao,fapesp,ffm)
     fs = f()
 
 #produto com ou sem IPI (1 ou 0)
-ipi = int(input("Valor IPI: "))
+ipi = int(input("Produto tem IPI?: "))
 if ipi>0:
     ipi = 1
 
 lado = lado_tabela(estado,contribuinte)
 
-operacao = notas()
+operacao = 1
+diff = int(input("Operacao diferente de venda normal? "))
+if diff != 0:
+    operacao = notas()
 
 
 tes = decisao(operacao,lado,ipi,fs,origem)
 
-print(tes)
+print("------------------------------")
+print("TES:",tes)
 
 
